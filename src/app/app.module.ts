ff01dd8f7ff5f374bd715a78d403e4b86e9c2735
hamayun <hamayun.waheed1999@gmail.com>
2021-06-01 21:43:03 +0500

    add interface-hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -10,7 +11,10 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     TopBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: TopBarComponent } 
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
