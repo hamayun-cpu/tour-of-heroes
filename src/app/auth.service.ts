2b63ed8b7e05dafda732fdf46026f2eaba07610e
hamayun <hamayun.waheed1999@gmail.com>
2021-06-08 17:41:39 +0500

    add auth.service and its logic

---
+++ src/app/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { tap, delay } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isUserLoggedIn = false;
+
+  login(userName: string, password: string) {
+    this.isUserLoggedIn = (userName == 'admin' && password == 'admin');
+    localStorage.setItem('isUserLoggedIn', this.isUserLoggedIn ? "true" : "false");
+
+    return of(this.isUserLoggedIn).pipe(
+      delay(1000),
+      tap(val => { 
+         console.log("Is User Authentication is successful: " + val); 
+      })
+   );
+  }
+
+  logout() {
+    this.isUserLoggedIn = false;
+    localStorage.removeItem('isUserLoggedIn'); 
+  }
+  
+  constructor() { }
+}
