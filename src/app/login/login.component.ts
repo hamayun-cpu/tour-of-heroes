34ff4b09d2df8c1d87eba0b30ae79fd04ab45fc6
hamayun <hamayun.waheed1999@gmail.com>
2021-06-10 02:27:45 +0500

    add login view

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  loginWithRedirect(): void{
+    this.auth.loginWithRedirect();
+  }
 }
