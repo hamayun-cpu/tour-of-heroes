c2f2534fd70292d79c3027059b668181a3267581
hamayun <hamayun.waheed1999@gmail.com>
2021-06-16 01:33:58 +0500

    define admin roles

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from '@auth0/auth0-angular';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -9,10 +10,12 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] | undefined;
+  signedInUser: any;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private auth: AuthService) { }
   ngOnInit() {
     this.getHeroes();
+    this.auth.user$.subscribe(user => { this.signedInUser = user });
   }
 
   getHeroes(){
@@ -20,12 +23,28 @@ export class HeroesComponent implements OnInit {
   }
 
   addHero(name: string){
-    if (name!== ''){
-      this.heroService.addHero(name);
+    if(this.isUserSignedIn()){
+      if (name!== ''){
+        this.heroService.addHero(name);
+      }
+    } else {
+      window.alert('Only ADMIN can add Heroes');
     }
   }
 
   delHero(hero: Hero) {
-    this.heroService.delHero(hero);
+    if(this.isUserSignedIn()){
+      this.heroService.delHero(hero);
+    } else {
+      window.alert('Only ADMIN can delete Heroes');
+    }
+  }
+
+  isUserSignedIn(){
+    if(this.signedInUser.email === 'hamayun.waheed1999@gmail.com'){
+      return true;
+    } else {
+      return false;
+    }
   }
 }
